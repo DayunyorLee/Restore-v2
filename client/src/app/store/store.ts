fd345d709f55aef25f79c345a31d79dd128e0385
Niel Aquino <niel.aquino.27@gmail.com>
2025-04-08 11:30:39 +1000

    End of section 6

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -3,6 +3,7 @@ import counterReducer, { CounterSlice } from "../../features/contact/counterRedu
 import { useDispatch, useSelector } from "react-redux";
 import { catalogApi } from "../../features/catalog/catalogApi";
 import { uiSlice } from "../layout/uiSlice";
+import { errorApi } from "../../features/about/errorApi";
 
 export function configureTheStore(){
     return legacy_createStore(counterReducer);
@@ -11,11 +12,12 @@ export function configureTheStore(){
 export const store = configureStore({
     reducer:{
         [catalogApi.reducerPath]: catalogApi.reducer,
+        [errorApi.reducerPath] : errorApi.reducer,
         counter1: CounterSlice.reducer,
         ui: uiSlice.reducer
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(catalogApi.middleware)
+        getDefaultMiddleware().concat(catalogApi.middleware, errorApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
